beb8d36556905d2ef223041ca6a46eb34866fe7d
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-06-27 14:42:39 -0400

    Create local cluster/client for all tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,19 @@
 from pathlib import Path
 
 import pytest
+from dask.distributed import Client, LocalCluster
 
 TEST_DIR = Path(__file__).parent
+
+
+@pytest.fixture(scope="session", name="dask_client", autouse=True)
+def dask_client():
+    """Create a single client for use by all unit test cases."""
+    cluster = LocalCluster(n_workers=1, threads_per_worker=1, dashboard_address=":0")
+    client = Client(cluster)
+    yield client
+    client.close()
+    cluster.close()
 
 
 @pytest.fixture
