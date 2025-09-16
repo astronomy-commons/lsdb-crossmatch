b115aa2660056249ae38047407987d54e3947ff6
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-09-16 11:44:52 -0400

    Initial passing test for nway.

---
+++ src/lsdb_crossmatch/nway/nway_crossmatch.py
@@ -0,0 +1,232 @@
+import hats.pixel_math.healpix_shim as hp
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+from lsdb.core.crossmatch.abstract_crossmatch_algorithm import AbstractCrossmatchAlgorithm
+from nwaylib import nway_match
+
+
+def _series_of(pa_dtype):
+    return pd.Series(dtype=pd.ArrowDtype(pa_dtype))
+
+
+class NWAYCrossmatch(AbstractCrossmatchAlgorithm):
+    """
+    Nway crossmatch algorithm.
+
+    match_tables: list of catalogues, each a dict with entries:
+    - name (short catalog name, no spaces, used in output columns)
+    - ra (RA in degrees)
+    - dec (dec in degrees)
+    - error (positional error in arcsec)
+    - area (sky area covered by the catalogue in square degrees)
+    - mags (list of additional columns to consider as priors)
+    - magnames (short name for each entry in mags, used in output columns)
+    - maghists (list of prior information for each entry in mags)
+      use None to automatically build a histogram of target/non-target sources.
+      Otherwise, supply the histogram manually: bins_lo, bins_hi, hist_sel, hist_all.
+
+    match_radius: maximum radius in arcsec to consider.
+        More distant counterparts are cut off.
+        Set to a very large value, e.g. 5 times the largest positional error.
+        Setting to larger values does not change the result, but
+        setting to smaller values improves performance.
+
+    prior_completeness: expected fraction of sources in the primary catalogue
+        that are expected to have a counterpart (e.g., 90%).
+        If an array is passed, completeness for each catalog. First
+        entry has to be 1.
+    """
+
+    extra_columns = pd.DataFrame(
+        {
+            "Catalog_separation": _series_of(pa.float64()),
+            "Separation_max": _series_of(pa.float64()),
+            "ncat": _series_of(pa.int64()),
+            "dist_bayesfactor_uncorrected": _series_of(pa.float64()),
+            "dist_bayesfactor": _series_of(pa.float64()),
+            "dist_post": _series_of(pa.float64()),
+            "p_single": _series_of(pa.float64()),
+            "match_flag": _series_of(pa.int64()),
+            "prob_has_match": _series_of(pa.float64()),
+            "prob_this_match": _series_of(pa.float64()),
+        }
+    )
+
+    @classmethod
+    def validate(
+        cls,
+        left,
+        right,
+        radius_arcsec,
+        pos_err_col_left=None,
+        pos_err_col_right=None,
+        ra_error_col_left=None,
+        dec_error_col_left=None,
+        ra_error_col_right=None,
+        dec_error_col_right=None,
+        left_mag_columns: list = None,
+        right_mag_columns: list = None,
+        prior_completeness=1,
+        match_flag=0,
+    ):
+        super().validate(left, right)
+        if radius_arcsec < 0:
+            raise ValueError("match radius has to be positive, you silly goose.")
+
+        left_has_pos_err = pos_err_col_left is not None
+        left_has_ra_dec_err = ra_error_col_left is not None and dec_error_col_left is not None
+
+        if left_has_pos_err:
+            if pos_err_col_left not in left.columns:
+                raise ValueError(f"Column '{pos_err_col_left}' not found in left catalog.")
+        elif left_has_ra_dec_err:
+            if not all(col in left.columns for col in [ra_error_col_left, dec_error_col_left]):
+                raise ValueError(f"column not found in left catalog.")
+        else:
+            raise ValueError(
+                "For left catalog, either positional error column OR ra error, dec error and dec must be provided."
+            )
+
+        right_has_pos_err = pos_err_col_right is not None
+        right_has_ra_dec_err = ra_error_col_right is not None and dec_error_col_right is not None
+
+        if right_has_pos_err:
+            if pos_err_col_right not in right.columns:
+                raise ValueError(f"Column '{pos_err_col_left}' not found in right catalog.")
+        elif right_has_ra_dec_err:
+            if not all(col in right.columns for col in [ra_error_col_right, dec_error_col_right]):
+                raise ValueError(f"column not found in right catalog.")
+        else:
+            raise ValueError(
+                "For right catalog, either positional error column OR ra error, dec error and dec must be provided."
+            )
+
+        if left_mag_columns is not None:
+            for col in left_mag_columns:
+                if col not in left.columns:
+                    raise ValueError(f"Column {col} not found in left catalog")
+
+        if right_mag_columns is not None:
+            for col in right_mag_columns:
+                if col not in right.columns:
+                    raise ValueError(f"Column {col} not found in right catalog")
+
+        if match_flag not in (0, 1, 2):
+            raise ValueError("`match_flag` must be an integer with value 0, 1, or 2")
+
+    def _construct_match_tables(self):
+        left_mags = []
+        if self.left_mag_columns is not None:
+            for col_name in self.left_mag_columns:
+                left_mags.append(self.left[col_name].to_numpy())
+
+        right_mags = []
+        if self.right_mag_columns is not None:
+            for col_name in self.right_mag_columns:
+                right_mags.append(self.right[col_name].to_numpy())
+
+        if self.pos_err_col_left is not None:
+            left_errors = self.left[self.pos_err_col_left].to_numpy()
+        else:
+            ra_err_left = self.left[self.ra_error_col_left].to_numpy()
+            dec_err_left = self.left[self.dec_error_col_left].to_numpy()
+            dec_left_radians = np.radians(self.left[self.left_catalog_info.dec_column].to_numpy())
+            left_errors = np.sqrt((ra_err_left * np.cos(dec_left_radians)) ** 2 + dec_err_left**2)
+
+        if self.pos_err_col_right is not None:
+            right_errors = self.right[self.pos_err_col_right].to_numpy()
+        else:
+            ra_err_right = self.right[self.ra_error_col_right].to_numpy()
+            dec_err_right = self.right[self.dec_error_col_right].to_numpy()
+            dec_right_radians = np.radians(self.right[self.right_catalog_info.dec_column].to_numpy())
+            right_errors = np.sqrt((ra_err_right * np.cos(dec_right_radians)) ** 2 + dec_err_right**2)
+
+        tables = [
+            {
+                "name": self.left_catalog_info.catalog_name,
+                "ra": self.left[self.left_catalog_info.ra_column].to_numpy(),
+                "dec": self.left[self.left_catalog_info.dec_column].to_numpy(),
+                "error": left_errors,
+                "area": hp.order2pixarea(self.left_order),
+                "mags": left_mags,
+                "magnames": self.left_mag_columns,
+                "maghists": [],
+            },
+            {
+                "name": self.right_catalog_info.catalog_name,
+                "ra": self.right[self.right_catalog_info.ra_column].to_numpy(),
+                "dec": self.right[self.right_catalog_info.dec_column].to_numpy(),
+                "error": right_errors,
+                "area": hp.order2pixarea(self.right_order),
+                "mags": right_mags,
+                "magnames": self.right_mag_columns,
+                "maghists": [],
+            },
+        ]
+        return tables
+
+    def perform_crossmatch(
+        self,
+        radius_arcsec,
+        pos_err_col_left=None,
+        pos_err_col_right=None,
+        ra_error_col_left=None,
+        dec_error_col_left=None,
+        ra_error_col_right=None,
+        dec_error_col_right=None,
+        left_mag_columns: list = None,
+        right_mag_columns: list = None,
+        prior_completeness=0.9,
+        match_flag=0,
+    ):
+        """Crossmatch two partitions"""
+
+        if left_mag_columns is None:
+            self.left_mag_columns = []
+        else:
+            self.left_mag_columns = left_mag_columns
+
+        if right_mag_columns is None:
+            self.right_mag_columns = []
+        else:
+            self.right_mag_columns = right_mag_columns
+
+        self.pos_err_col_left = pos_err_col_left
+        self.pos_err_col_right = pos_err_col_right
+
+        self.ra_error_col_left = ra_error_col_left
+        self.dec_error_col_left = dec_error_col_left
+
+        self.ra_error_col_right = ra_error_col_right
+        self.dec_error_col_right = dec_error_col_right
+
+        match_tables = self._construct_match_tables()
+        results = nway_match(match_tables, radius_arcsec, prior_completeness)
+
+        old_sep_col_name = (
+            f"Separation_{self.left_catalog_info.catalog_name}_{self.right_catalog_info.catalog_name}"
+        )
+
+        results = results.rename(columns={old_sep_col_name: "Catalog_separation"})
+
+        for col_name, expected_dtype in self.extra_columns.dtypes.items():
+            if col_name in results.columns and results[col_name].dtype != expected_dtype:
+                results[col_name] = results[col_name].astype(expected_dtype)
+
+        results = results.reset_index(drop=True)
+
+        results = results.query(f"{self.right_catalog_info.catalog_name} != -1")
+
+        if match_flag == 1:
+            results = results.query("match_flag == 1")
+        elif match_flag == 2:
+            results = results.query("match_flag != 0")
+
+        left_idx = results[self.left_catalog_info.catalog_name].to_numpy()
+        right_idx = results[self.right_catalog_info.catalog_name].to_numpy()
+
+        results = results.drop(
+            columns=[self.left_catalog_info.catalog_name, self.right_catalog_info.catalog_name]
+        )
+        return left_idx, right_idx, results
---
+++ tests/lsdb_crossmatch/nway/test_nway_crossmatch.py
@@ -0,0 +1,35 @@
+import lsdb
+import nested_pandas as npd
+import pandas as pd
+import pytest
+
+from lsdb_crossmatch.nway.nway_crossmatch import NWAYCrossmatch
+
+pd.set_option("display.max_rows", None)
+
+
+def test_mag_difference_crossmatch(m67_delve_small_dir, m67_ps1_small_dir, xmatch_mags):
+    small_ps1 = lsdb.open_catalog(m67_ps1_small_dir)
+    small_delve = lsdb.open_catalog(m67_delve_small_dir)
+
+    xmatched = lsdb.crossmatch(
+        small_ps1,
+        small_delve,
+        suffixes=("_ps1", "_delve"),
+        algorithm=NWAYCrossmatch,
+        radius_arcsec=60,
+        left_mag_columns=["gMeanPSFMag", "rMeanPSFMag", "iMeanPSFMag", "zMeanPSFMag"],
+        right_mag_columns=["MAG_PSF_G", "MAG_PSF_R", "MAG_PSF_I", "MAG_PSF_Z"],
+        ra_error_col_left="raMeanErr",
+        dec_error_col_left="decMeanErr",
+        ra_error_col_right="MAGERR_PSF_G",  ## Well beans
+        dec_error_col_right="MAGERR_PSF_I",
+        match_flag=2,
+    ).compute()
+
+    assert isinstance(xmatched, npd.NestedFrame)
+
+    for _, correct_row in xmatch_mags.iterrows():
+        assert correct_row["id_ps1"] in xmatched["objID_ps1"].to_numpy()
+        xmatch_row = xmatched[xmatched["objID_ps1"] == correct_row["id_ps1"]]
+        assert correct_row["id_delve"] in xmatch_row["QUICK_OBJECT_ID_delve"].to_numpy()
